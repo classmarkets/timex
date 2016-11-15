7927d1f8180dd8761234178ac0aac66ead5bf3be
Peter Schultz <peter.schultz@classmarkets.com>
2016-11-15 12:01:53 +0100

    add failing tests for end of febuary

--- duration_test.go
+++ duration_test.go
@@ -176,6 +176,36 @@ func TestAddMonths(t *testing.T) {
 			want:   time.Date(2016, 10, 1, 8, 0, 0, 0, berlin),
 			months: -1,
 		},
+		"february -> march": {
+			t:      time.Date(2015, 02, 28, 8, 0, 0, 0, berlin),
+			want:   time.Date(2015, 03, 31, 8, 0, 0, 0, berlin),
+			months: 1,
+		},
+		"march -> february": {
+			t:      time.Date(2015, 03, 31, 8, 0, 0, 0, berlin),
+			want:   time.Date(2015, 02, 28, 8, 0, 0, 0, berlin),
+			months: -1,
+		},
+		"february 28th -> march (leap year)": {
+			t:      time.Date(2016, 02, 28, 8, 0, 0, 0, berlin),
+			want:   time.Date(2016, 03, 28, 8, 0, 0, 0, berlin),
+			months: 1,
+		},
+		"march -> february 28th (leap year)": {
+			t:      time.Date(2016, 03, 28, 8, 0, 0, 0, berlin),
+			want:   time.Date(2016, 02, 28, 8, 0, 0, 0, berlin),
+			months: -1,
+		},
+		"february 29th -> march (leap year)": {
+			t:      time.Date(2016, 02, 29, 8, 0, 0, 0, berlin),
+			want:   time.Date(2016, 03, 31, 8, 0, 0, 0, berlin),
+			months: 1,
+		},
+		"march -> february 29th (leap year)": {
+			t:      time.Date(2016, 03, 31, 8, 0, 0, 0, berlin),
+			want:   time.Date(2016, 02, 29, 8, 0, 0, 0, berlin),
+			months: -1,
+		},
 	}
 
 	for name, c := range cases {
